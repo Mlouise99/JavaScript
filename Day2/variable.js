60cf209ba63e3df89bb7515fd2ee8c348561dfe2
Mlouise99 <mwubahalouise99@gmail.com>
2024-02-26 13:18:50 +0200

    the variable and datatypes

---
+++ Day2/variable.js
@@ -0,0 +1,31 @@
+const age= 10;
+const myName= "Louise";
+const isSheAgirl = true;
+const array = ["string", 12, false];
+const myFavFood=["bean","maize", "meat"];
+console.log("my name is " + myName + "and am " + age + "years old");
+console.log('my name is ${myName} and am ${age} years old');
+console.log(array);
+console.log(myFavFood);
+ function myLastName(){
+    let me = "Mwubahama";
+    console.log(me);
+ }
+var me= "girls";
+console.log(typeof me);
+
+var me= "null";
+console.log(typeof me);
+// NoN means not a number and NaN()method converts the value to a number before testing it.
+let str = true;
+let b = 12;
+console.log(str - b);
+
+
+
+
+
+
+
+
+
