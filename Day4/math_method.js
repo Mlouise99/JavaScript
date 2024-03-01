18f460a66284a4e5956a2a80417b2c97bbe2a20e
Mlouise99 <mwubahalouise99@gmail.com>
2024-03-01 21:17:35 +0200

    for the math method

---
+++ Day4/math_method.js
@@ -0,0 +1,23 @@
+// Math Method
+const num =  -100
+console.log(Math.abs(num));
+const number = 9.27890
+console.log(Math.ceil(number));
+console.log(Math.floor(number));
+console.log(Math.random(number));
+console.log(Math.round(number));
+console.log(Math.trunc(number));
+let maximun = Math.max(20, 40, 79, 10, 1, 578);
+console.log(maximun);
+let x = Math.sqrt(25);
+console.log(x);
+let y =8
+let z = 10
+power= Math.pow(y,z);
+console.log(power);
+
+let logValue = Math.log(7);
+let expValue = Math.exp(2);
+console.log(logValue); 
+console.log(expValue);
+
